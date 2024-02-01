4587e6ffee1aa162df3dfc02a5042c9f12b144b5
shiva <guru_shiva1625@gmail.com>
2024-02-01 19:30:05 +0530

    Add font awesome

--- components/Noteitem.jsx
+++ components/Noteitem.jsx
@@ -6,7 +6,19 @@ const Noteitem = (props) => {
     <div className="col-md-3">
       <div className="card my-2">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
+          <div className="d-flex align-items-centers">
+            <h5 className="card-title">{note.title}</h5>
+            <div className="mx-2">
+              <i
+                className="far fa-trash-alt mx-2 my-1"
+                style={{ cursor: "pointer" }}
+              ></i>
+              <i
+                className="far fa-edit mx-2 my-1"
+                style={{ cursor: "pointer" }}
+              ></i>
+            </div>
+          </div>
           <p className="card-text">{note.description}</p>
         </div>
       </div>
